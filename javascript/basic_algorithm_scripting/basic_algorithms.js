a7ff85406e0829df5bc193919ee399b90bf5a60d
Khalal Walker <khalal.walker@gmail.com>
2018-09-14 22:01:44 -0700

    commit new algorithm challenges

--- javascript/basic_algorithm_scripting/basic_algorithms.js
+++ javascript/basic_algorithm_scripting/basic_algorithms.js
@@ -7,8 +7,7 @@ function convertToF(celsius) {
 // Reverse a string
 function reverseString(str) {
     //turn string into an array, reverse the array, join to make a string, remove commas
-    let reverse = str.split("").reverse().join().replace(/,/g, "");
-    return reverse;
+    return str.split("").reverse().join().replace(/,/g, "");
 }
 
 // Factorialize a number
@@ -53,7 +52,7 @@ function largestOfFour(arr) {
 
 // Confirm the ending of a String
 function confirmEnding(str, target) {
-    return str.substring(str.length - target.length) == target;
+    return str.slice(str.length - target.length) == target;
 }
 
 // Repeat a string
@@ -65,4 +64,49 @@ function repeatStringNumTimes(str, num) {
         counter++;
     }
     return repeatStr;
+}
+
+// Truncate a String
+function truncateString(str, num) {
+    if (str.length > num) {
+        return str.slice(0, num) + "...";
+    }
+    else {
+        return str;
+    }
+} 
+
+// Looks through an array and returns first element to pass the truth test
+function findElement(arr, func) {
+    for (let i = 0; i < arr.length; i++) {
+       if (func(arr[i])) {
+           return arr[i];
+       }
+    }
+}
+
+// Check boolean primitive
+function booWho(bool) {
+    return typeof bool == 'boolean';
+}
+
+// Title Case Sentence
+function titleCase(str) {
+    let titleString = str.toLowerCase().split(" ");
+    for(let i = 0; i < titleString.length; i++) {
+        titleString[i] = titleString[i].replace(/^\w/, character => character.toUpperCase());
+    }
+    return titleString.join().replace(/,/g, " ");
+}
+
+// Franken Splice
+function frankenSplice(arr1, arr2, n) {
+    let arr = arr2.slice();
+   arr.splice(n, 0, ...arr1);
+   return arr;
+}
+
+// Falsy Bouncer
+function bouncer(arr) {
+    return arr.filter(value => Boolean);
 }
