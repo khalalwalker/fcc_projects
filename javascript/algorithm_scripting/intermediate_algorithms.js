1acc3c809bd89418fb1c08726de700d535083886
Khalal Walker <khalal.walker@gmail.com>
2018-09-23 22:11:15 -0700

    commiting four challenges

--- javascript/algorithm_scripting/intermediate_algorithms.js
+++ javascript/algorithm_scripting/intermediate_algorithms.js
@@ -72,3 +72,79 @@ function myReplace(str, before, after) {
     newString = newString.join(" ");
     return newString;
 }
+
+// DNA Pairing
+function pairElement(str) {
+
+    let pair = str.split("");
+    for(let i = 0; i < pair.length; i++) {
+        if (pair[i] === "A") {
+            pair[i] = ["A", "T"];
+        }
+        else if (pair[i] === "T") {
+            pair[i] = ["T", "A"];
+        }
+        else if (pair[i] === "C") {
+            pair[i] = ["C", "G"];
+        }
+        else if (pair[i] === "G") {
+            pair[i] = ["G", "C"];
+        }
+    }
+    return pair;
+}
+
+// Missing Letters
+function fearNotLetter(str) {
+    for (let i = 1; i < str.length; i++) {
+        // If there is more than a 1 letter difference between adjacent letters.
+        if ((str.charCodeAt(i) - str.charCodeAt(i - 1)) > 1) {
+            return String.fromCharCode(str.charCodeAt(i - 1) + 1);
+        }
+    }
+    return undefined;
+}
+
+// Convert HTML entities
+function convertHTML(str) {
+    let newString = str.split("");
+    for (let i = 0; i < newString.length; i++) {
+        switch (newString[i]) {
+            case "&":
+                newString[i] = "&amp;";
+                break;
+            case "<":
+                newString[i] = "&lt;";
+                break;
+            case ">":
+                newString[i] ="&gt;";  
+                break;
+            case '"':
+                newString[i] = "&quot;";
+                break;
+            case "'":
+                newString[i] = "&apos;";
+                break;
+        }
+    }
+
+    return newString.join("");
+}
+
+// Sum of All Odd Fibonacci Numbers
+function sumFibs(num) {
+    let sum = 0;
+    let prev = 0;
+    let temp = 0;
+    let current = 1;
+    while (current <= num) {
+        if (current % 2 !== 0) {
+            sum += current;
+        }
+        temp = current;
+        current += prev;
+        prev = temp;
+    }
+    return sum;
+}
+
